3c3a8973b0bd05a47a5cbdc4aca64083c7fea174
Brett J. Butler <bbutler@eco.umass.edu>
2018-12-03 15:28:50 -0500

    20181203

--- R/nwos_mean.R
+++ R/nwos_mean.R
@@ -1,12 +1,15 @@
 #' NWOS Mean
 #'
-#' This function calculates means for the NWOS.
-#' @usage nwos_mean(weight, area = 1, domain = 1, base = 1, variable)
-#' @param weight vector of weights per ownership.
-#' @param area vector of area (e.g., forest acres) per ownership. Default = 1 (i.e., estimates are in terms of ownerships).
-#' @param domain vector with 1 indicating inclusion in the domain and 0 otherwise. Default = 1 (i.e., all ownerships are included).
-#' @param variable vector of variable of interest.
+#' Calculate means for the NWOS.
+#' @usage nwos_mean(weight, area = 1, domain = 1, variable)
+#' @param weight vector of weights.
+#' @param area vector of area (e.g., forest acres in the stratum). Default = 1 (i.e., estimates are in terms of ownerships).
+#' @param domain binary vector (i.e., dummay variable) with 1 indicating inclusion in the domain  of interest and 0 otherwise. Default = 1 (i.e., all ownerships are included).
+#' @param variable vector of values for the variable of interest.
+#' @return Mean of variable of interest.
 #' @keywords nwos
+#' @details
+#' This function needs to be run by stratum (e.g., family forest ownerships in a state).
 #' @export
 #' @references
 #' Butler, B.J. In review. Weighting for the US Forest Service, National Woodland Owner Survey. U.S. Department of Agriculture, Forest Service, Northern Research Station. Newotwn Square, PA.
@@ -14,7 +17,7 @@
 #' nwos_mean(weight = wi$WEIGHT, variable = wi$Y_3)
 #' nwos_mean(weight = wi$WEIGHT, area = wi$AC_WOOD, variable = wi$Y_3)
 
-nwos_mean <- function(weight, area = 1, domain = 1, base = 1, variable)
+nwos_mean <- function(weight, area = 1, domain = 1, variable)
 {
   nwos_total(weight = weight, area = area, domain = domain, variable = variable) /
     nwos_total(weight = weight, area = area, domain = domain, variable = 1)
--- R/nwos_mean_apply.R
+++ R/nwos_mean_apply.R
@@ -1,18 +1,20 @@
 #' NWOS Proportion
 #'
-#' This function calculates proportions for the NWOS replicates. This is typically used with an apply function.
+#' This function calculates means for NWOS replicates. This is typically used with an apply function.
 #' @usage nwos_proportion_replicates(r, index.rep, data, weight.rep, owner.area.name = "OWNER", domain.name, base.name = "FFO", variable.name = "FFO")
-#' @param r vector of replicates. See details below.
-#' @param index.rep list of observations (i.e., replicates) in data to include.
-#' @param data data frame containing stratum variable.
-#' @param weight.rep list with each element the weights for the observations in that replicate.
-#' @param owner.area.name nae of a variable in data of the area (of forestland) owned by each ownership. Default = "OWNER" (i.e., 1, which is used for estimating number of ownerships).
-#' @param domain.name name of the variable in data indicating inclusion (1) and exclusion (0) in the domain of interest.
-#' @param base.name name of a variable in data with 1 indicating inclusion in the base (i.e., denominator) and 0 otherwise. Default = 1 (i.e., all ownerships are included).
-#' @param variable.name name of the variable of interest in data. Default = "FFO".
-#' @keywords nwos
-#' @details
+#' @param r vector of replicates. Typically 1:length(index).
+#' @param index list of rows to include in each replicate.
+#' @param weight vector of weights.
+#' @param area vector of area (e.g., forest acres in the stratum).
+#' @param domain binary vector (i.e., dummay variable) with 1 indicating inclusion in the domain of interest and 0 otherwise.
+#' @param variable vector of values for the variable of interest.
+#' @return Mean of variable of interest.
+#'  @keywords nwos
+#' @seealso nwos_mean
 #' @export
+#' @details
+#' This function needs to be run by stratum (e.g., family forest ownerships in a state).
+#' Due to indexing (to allow for apply function), there are fewer defauly values than nwos_mean.
 #' @references
 #' Butler, B.J. In review. Weighting for the US Forest Service, National Woodland Owner Survey. U.S. Department of Agriculture, Forest Service, Northern Research Station. Newotwn Square, PA.
 #' @examples
