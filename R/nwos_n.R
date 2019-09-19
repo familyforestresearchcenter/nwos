56b704e3cfcba148c4b9e88b1215a0fa78d08874
Brett J. Butler <bbutler01@1x-vl946-128-119-249-190.wireless.umass.edu>
2019-09-19 09:11:12 -0400

    test

--- R/nwos_n.R
+++ R/nwos_n.R
@@ -5,7 +5,7 @@
 #' @param point.count vector of number of sample points associated with each observation. Default = 1.
 #' @param stratum vector with 1 indicating inclusion in the stratum and 0 otherwise. Default = 1 (i.e., all ownerships are in the same stratum).
 #' @param domain vector with 1 indicating inclusion in the domain and 0 otherwise. Default = 1 (i.e., all ownerships are included).
-#' @param variable vector of variable of interest. Default = 1 (i.e., variable is ignored).
+#' @param variable vector of variable of interest. Default = 1 (i.e., variable is ignored)
 #' @keywords nwos
 #' @export
 #' @references
