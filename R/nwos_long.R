171f48ddf5fa2bc16b0707506c79e205d49257ec
JesseCaputo <JesseCaputo@users.noreply.github.com>
2020-01-27 10:11:49 -0500

    adds imputations
    Add imputations/weights to get_long() and imputations parameter to nwos_export()

--- R/nwos_long.R
+++ R/nwos_long.R
@@ -3,21 +3,39 @@
 #' Formats NWOS data into a long format for analysis
 #'
 #' @param x is a nwos.object
+#' @param imputations is string containing which imputation set is to be used in place of non-response, if any. Valid responses are "none","random","1","2","3","4","5"
 #'
 #' @return a data.frame
 #'
 #' @examples
-#' nwos_long(foo)
+#' nwos_long(foo,imputations="1")
 #'
 #' @export
 
-nwos_long <- function(x){
-  
+nwos_long <- function(x, imputations="none"){
+
   if(!is(x,'nwos.object')){
     stop("nwos_long() requires nwos.object as input")
   }
-  
-  l <- merge(x@sample,x@quest,by="CN")
+
+  if(!imputations %in% c('none','random',1:5)){
+    stop("'imputations' only excepts values of 'none','random', or integer")
+  }
+
+  st <- x@quest #extract quest
+  if (imputations!='none'){
+    if (imputations=='random'){ #insert imputations, if wanted
+      rep <- sample(x@imputations$IMPUTATION,1) #choose random imputation
+    } else {
+      rep <- imputations #choose selected imputation
+    }
+    imp <- x@imputations[x@imputations$IMPUTATION==rep,] #subset imputation
+    st.UK <- paste(st$CN,st$METADATA_CN,sep='_') #create keys
+    imp.UK <- paste(imp$RESPONSE_CN,imp$METADATA_CN,sep='_')
+    TR <- st.UK %in% imp.UK #to replace, those records with imputed values
+    st$RESPONSE_VALUE[TR] <- imp$RESPONSE_VALUE[match(st.UK[TR],imp.UK)] #insert value
+  }
+  l <- merge(x@sample,st,by="CN")
   #reformat l
   for (i in 1:length(l)){ #cast columns as correct datatype
     t <- x@fields$DATA_TYPE[match(names(l)[i],x@fields$FIELD_NAME)]
@@ -27,5 +45,7 @@ nwos_long <- function(x){
       l[,i] <- as.integer(l[,i])
     }
   }
+  l$PLOT_COUNT <- x@weights$PLOT_COUNT[match(l$CN,x@weights$RESPONSE_CN)]
+  l$FINAL_WEIGHT <- x@weights$FINAL_WEIGHT[match(l$CN,x@weights$RESPONSE_CN)]
   return(l)
 }
--- R/nwos_long_metadata.R
+++ R/nwos_long_metadata.R
@@ -12,11 +12,17 @@
 #' @export
 
 nwos_long_metadata <- function(x){
-  
+
   if(!is(x,'nwos.object')){
     stop("nwos_long_metadata() requires nwos.object as input")
   }
-  
-  md <- x@fields[x@fields$FIELD_NAME %in% names(x@quest),]
+
+  md <- x@fields[x@fields$FIELD_NAME %in% c(names(x@quest),names(x@sample)),]
+  #add weights column
+  md <- rbind(md,
+              data.frame(FIELD_NAME=c('POINT_COUNT','FINAL_WEIGHT'),
+                         DESCRIPTION=c('Number of plots associated with this survey','Sampling weight, for estimates and models'),
+                         DATA_TYPE=c('NUMERIC','NUMERIC'),
+                         UNITS_FACTORS=c('PLOTS',NA)))
   return(md)
 }
