bb458ecf6c8f50df766df4e81c2ba020ad4103d0
Brett J Butler <bbutler@eco.umass.edu>
2020-07-10 08:20:26 -0400

    Update

--- R/nwos_estimates_combine.R
+++ R/nwos_estimates_combine.R
@@ -5,7 +5,7 @@
 #' @export
 #'
 
-nwos_estimates_combine <- function(stratum = "FFO", domain = "ONEPLUS") {
+nwos_estimates_combine <- function(stratum = "FFO", domain = "ONEPLUS", wd = "DATA") {
   require(tidyverse)
   require(parallel)
   N_CORES <- detectCores() - 1
@@ -14,10 +14,10 @@ nwos_estimates_combine <- function(stratum = "FFO", domain = "ONEPLUS") {
   # files <- list.files("/home/ubuntu/SUPP", full.names = T)
   # files <- list.files("~/Dropbox (FFRC)/NWOS/ESTIMATION/ESTIMATES/DATA/FFO/TENPLUS/SUPPLEMENT/REP", full.names = T)
 
-  files <- list.files("DATA", pattern = ".RDS")
+  files <- list.files(paste0(wd, "/REP"), pattern = ".RDS")
   files <- files[!files %in% c("QUEST_20200212.RDS", "REP_WEIGHTS_20200212.RDS")]
 
-  data <- readRDS(paste0("DATA/",files[1]))
+  data <- readRDS(paste0(wd, "/",files[1]))
   geo.list <- data %>% distinct(GEO) %>% pull()
 
   write_geo_rep <- function(geo = "US") {
@@ -29,14 +29,15 @@ nwos_estimates_combine <- function(stratum = "FFO", domain = "ONEPLUS") {
     #                          stratum, "_", domain, "_", rep, "_", geo, ".RDS"))
     # saveRDS(data.geo, paste0("DATA/FFO/TENPLUS/SUPPLEMENT/REP_GEO/" ,
     #                          stratum, "_", domain, "_", rep, "_", geo, ".RDS"))
-    saveRDS(data.geo, paste0("DATA/REP_GEO/" ,
-                             stratum, "_", domain, "_", rep, "_", geo, ".RDS"))
+    # saveRDS(data.geo, paste0("DATA/REP_GEO/" ,
+    #                          stratum, "_", domain, "_", rep, "_", geo, ".RDS"))
+    saveRDS(data.geo, paste0(wd, "/REP_GEO/" , stratum, "_", domain, "_", rep, "_", geo, ".RDS"))
   }
 
   for(i in files) {
     print(i)
     # data <- readRDS(i)
-    data <- readRDS(paste0("DATA/", i))
+    data <- readRDS(paste0(wd, "/REP/", i))
 
     invisible(mclapply(geo.list, write_geo_rep, mc.cores = N_CORES))
   }
@@ -44,7 +45,8 @@ nwos_estimates_combine <- function(stratum = "FFO", domain = "ONEPLUS") {
   # files.geo.list <- list.files(paste0("DATA/", stratum, "/", domain, "/REP_GEO"), pattern = ".RDS", full.names = T)
   # files.geo.list <- list.files("REP_GEO", pattern = ".RDS", full.names = T)
   # files.geo.list <- list.files("DATA/FFO/TENPLUS/SUPPLEMENT/REP_GEO", pattern = ".RDS", full.names = T)
-  files.geo.list <- list.files("DATA/REP_GEO", pattern = ".RDS", full.names = T)
+  # files.geo.list <- list.files("DATA/REP_GEO", pattern = ".RDS", full.names = T)
+  files.geo.list <- list.files(paste0(wd, "/REP_GEO"), pattern = ".RDS", full.names = T)
 
   # If too large, may be able to convert to wide
   for(i in geo.list) { # 1:64 [55]
@@ -53,13 +55,21 @@ nwos_estimates_combine <- function(stratum = "FFO", domain = "ONEPLUS") {
     data <- bind_rows(mclapply(files.geo,
                                function(x) {readRDS(x) %>% mutate(REP = as.numeric(REP))},
                                mc.cores = N_CORES))
+    # data <- tibble()
+    # for(j in files.geo) {
+    #   print(j)
+    #   data <- data %>%
+    #     bind_rows(readRDS(j) %>% mutate(REP = as.numeric(REP)))
+    # }
+
     # saveRDS(data, paste0("DATA/", stratum, "/", domain, "/GEO/" ,
     #                      stratum, "_", domain, "_", i, ".RDS"))
     # saveRDS(data, paste0("GEO/" ,
     #                      stratum, "_", domain, "_", i, ".RDS"))
     # saveRDS(data, paste0("DATA/FFO/TENPLUS/SUPPLEMENT/GEO/" ,
     #                      stratum, "_", domain, "_", i, ".RDS"))
-    saveRDS(data, paste0("DATA/GEO/" ,
-                         stratum, "_", domain, "_", i, ".RDS"))
+    # saveRDS(data, paste0("DATA/GEO/" ,
+    #                      stratum, "_", domain, "_", i, ".RDS"))
+    saveRDS(data, paste0(wd, "/GEO/" , stratum, "_", domain, "_", i, ".RDS"))
   }
 }
--- R/nwos_table_area.R
+++ R/nwos_table_area.R
@@ -3,14 +3,14 @@
 #' Create the body of an NWOS core, area, or cooperataion rate table
 #' @export
 #' @details For area and cooperation rate tables see ...
-#' geo.abb = "AL"
+#' geo.abb = "US"
 #' data = AREA
 #' yr = year
 #' yr.range = year.range
 #' ref.tab = REF_TABLE
 #' ref.geo = REF_GEO
 #' nwos_table()
-#' nwos_table_area(geo.abb = "AL")
+#' nwos_table_area(geo.abb = "US")
 #' nwos_table_coop()
 
 nwos_table_area <- function(geo.abb, data = AREA,
@@ -22,9 +22,17 @@ nwos_table_area <- function(geo.abb, data = AREA,
   geo.name <- ifelse(ref.geo$GEO_ABB %in% c("US", "WEST", "PACIFIC_COAST"),
                      paste0(ref.geo$GEO_NAME, "$^{*}$"), ref.geo$GEO_NAME)
 
-  data <- data %>% filter(GEO_ABB %in% geo.abb)
-  total.acres <- data %>% filter(OWNCAT %in% "ALL") %>% pull(ACRES)
-  data <- data %>% mutate(PROPORTION = ACRES / total.acres)
+  data <- data %>% filter(GEO_ABB %in% geo.abb) %>%
+    select(OWNGRP, ACRES, ACRES_VARIANCE)
+  data <- bind_rows(data,
+                    tibble(OWNGRP = "Private",
+                           data %>% filter(OWNGRP %in% c("Family", "Corporate", "Other private")) %>%
+                             summarize_at(vars(ACRES, ACRES_VARIANCE), sum)),
+                    tibble(OWNGRP = "Public",
+                           data %>% filter(OWNGRP %in% c("Federal", "State", "Local")) %>%
+                             summarize_at(vars(ACRES, ACRES_VARIANCE), sum)),
+                    tibble(OWNGRP = "Total",
+                           data %>% summarize_at(vars(ACRES, ACRES_VARIANCE), sum)))
 
   begin.tex <- c("\\pagebreak",
                  "\\begin{minipage}{6.5in}",
@@ -43,70 +51,60 @@ nwos_table_area <- function(geo.abb, data = AREA,
   body <- c("\\begin{center}",
             "\\begin{tabular}{l rr}",
             "\\toprule",
-            "\\textbf{\\specialcell{Ownership\\\\category}} & \\textbf{Acres} & \\textbf{SE$^a$} \\\\", # & \\textbf{Percentage}
+            "\\textbf{\\specialcell{Ownership\\\\category}} & \\textbf{Acres} & \\textbf{SE$^a$} \\\\",
             '\\midrule',
             "& \\multicolumn{2}{c}{\\textit{- - thousands - -}} \\\\",
             "Private \\\\",
             "\\rowcolor{gray!25}",
             paste0("\\hspace{0.5em} Family  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "FAMILY") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "FAMILY") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "FAMILY") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Family") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Family") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             paste0("\\hspace{0.5em} Corporate  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "CORPORATE") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "CORPORATE") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "CORPORATE") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Corporate") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Corporate") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             "\\rowcolor{gray!25}",
             paste0("\\hspace{0.5em} Other private  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "OTHER_PRIVATE") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "OTHER_PRIVATE") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "OTHER_PRIVATE") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Other private") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Other private") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             "\\cmidrule{2-3}",
             paste0("\\hspace{0.5em} Total private  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "PRIVATE") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "PRIVATE") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "PRIVATE") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Private") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Private") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             "Tribal \\\\",
             "\\rowcolor{gray!25}",
             paste0("\\hspace{0.5em} Tribal  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "TRIBAL") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "TRIBAL") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "TRIBAL") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Tribal") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Tribal") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             "Public \\\\",
             "\\rowcolor{gray!25}",
             paste0("\\hspace{0.5em} Federal  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "FEDERAL") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "FEDERAL") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "FEDERAL") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Federal") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Federal") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             paste0("\\hspace{0.5em} State  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "STATE") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "STATE") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "STATE") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "STATE") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "STATE") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             '\\rowcolor{gray!25}',
             paste0("\\hspace{0.5em} Local  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "LOCAL") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "LOCAL") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "LOCAL") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Local") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Local") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             "\\cmidrule{2-3}",
             paste0("\\hspace{0.5em} Total public  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "PUBLIC") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "PUBLIC") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "PUBLIC") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Public") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Public") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             "\\cmidrule{2-3}",
             "\\rowcolor{gray!25}",
             paste0("\\hspace{0.5em} Total  & ",
-                   nwos_table_number(data %>% filter(OWNCAT %in% "ALL") %>% pull(ACRES) / 1e3), "  & ",
-                   nwos_table_number(sqrt(data %>% filter(OWNCAT %in% "ALL") %>% pull(VARIANCE)) / 1e3), #"  & ",
-                   # nwos_table_number(data %>% filter(OWNCAT %in% "ALL") %>% pull(PROPORTION) * 100, d = 1),
+                   nwos_table_number(data %>% filter(OWNGRP %in% "Total") %>% pull(ACRES) / 1e3), "  & ",
+                   nwos_table_number(sqrt(data %>% filter(OWNGRP %in% "Total") %>% pull(ACRES_VARIANCE)) / 1e3),
                    " \\\\"),
             "\\bottomrule \\\\",
             "\\end{tabular}",
