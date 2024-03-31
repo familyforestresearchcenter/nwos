73deafd15326669ea0c22ba20fbdf6448dc66840
Brett J Butler <bbutler@eco.umass.edu>
2024-03-31 07:53:21 -0400

    Update nwos_urban_table_number.R
    Fix formatting of numbers less than 1 with decimal place(s)

--- R/nwos_urban_table_number.R
+++ R/nwos_urban_table_number.R
@@ -1,6 +1,7 @@
 #' nwos_urban_table_number
 #'
-#' Converts units (e.g., to thousands) and rounds simialr to base except it adds another digit for values less than 1
+#' Converts units (e.g., to thousands) and rounds similar to base except it adds another digit for values less than 1
+#' @param x value to be formatted
 #' @param d
 #' @param r
 #' @param n
@@ -9,7 +10,9 @@
 #' nwos_urban_table_number(1100, r = -3)
 #' nwos_urban_table_number(900)
 #' nwos_urban_table_number(900, r = -3)
+#' nwos_urban_table_number(1.1, d = 1)
 #' nwos_urban_table_number(0.9)
+#' nwos_urban_table_number(0.9, d = 1)
 #' nwos_urban_table_number(0)
 #' nwos_urban_table_number(0, n = 0)
 #' nwos_urban_table_number(NA)
@@ -20,5 +23,6 @@ nwos_urban_table_number <- function(x, d = 0, r = 0, n = 1, n.max = Inf, less.on
     length(x) == 0 | is.na(x) ~ "--",
     n.zero & (n == 0 | n == n.max)~ "--",
     less.one & x < (1 * 10^abs(r)) ~ paste0("<", formatC(1 * 10^abs(r), big.mark = ",", format = "f", digits = d)),
+    d == 1 & r == 0 ~ formatC(round(x, 1), big.mark = ",", format = "f", digits = d),
     TRUE ~ formatC(round(x, r), big.mark = ",", format = "f", digits = d))
 }
